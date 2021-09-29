976375a02666ab6df37b7ac5a56559804d67516e
Kartheek1508 <bnskartheek15168@gmail.com>
2021-09-29 14:04:57 +0530

    Update echo cog.

--- echo/echo.py
+++ echo/echo.py
@@ -1,18 +1,13 @@
 from redbot.core import commands
 
-class Echo(commands.Cog):
-    """echo"""
+class TestCog(commands.Cog):
+    """test_cog"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        
-        if message.content.startswith('b.15082007'):
-            await ctx delete
-
-            await ctx(message.content[10:])
-
-        await ctx.send("I can do stuff!")
+    async def mycom2(self, ctx, *, text):
+        """This does more stuff!"""
+        await ctx.message.delete()
+        await ctx.send(text)
