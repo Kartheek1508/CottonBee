ade1402663e8bad54f2ea9727152af8d511dedbe
Kartheek1508 <bnskartheek15168@gmail.com>
2021-09-30 17:31:52 +0530

    Only admin without name and @ for normal users

--- echo/echo.py
+++ echo/echo.py
@@ -1,4 +1,5 @@
-from redbot.core import commands
+from redbot.core import commands, checks
+import discord 
 
 class Echo(commands.Cog):
     """echo"""
@@ -8,6 +9,10 @@ class Echo(commands.Cog):
 
     @commands.command()
     async def echo(self, ctx, *, text):
-        """This does more stuff!"""
+        "Echo what you said (only for admins)"
         await ctx.message.delete()
-        await ctx.send(text)
+        if await self.bot.is_admin(ctx.author):
+            response = text
+        else:
+            response = f"{text}  - <@{ctx.author.id}>"
+        await ctx.send(response, allowed_mentions=discord.AllowedMentions.none())        
